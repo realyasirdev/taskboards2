aa712a12ad354721577f23bb8488903db579200b
realyasirdev <yasiralikhanrazakhail@gmail.com>
2026-05-05 16:16:59 +0500

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.use(cors({ origin: "http://localhost:5173" })); ///CORS
+app.use(express.json());
+app.use(rateLimiter);
+
+app.use("/api/notes", notesRoutes);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
